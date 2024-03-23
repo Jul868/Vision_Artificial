3ad1857779d54978ba276a253cbc1baf1628f84d
Jul868 <julianaa2708@gmail.com>
2024-03-22 20:29:28 -0500

    changes

--- Quiz1/gui.py
+++ Quiz1/gui.py
@@ -13,21 +13,18 @@ import cv2
 import numpy as np
 import reportLog
 import loadImage
+import math
 
 class App(ttk.Frame):
     def __init__(self, master=None):
         try:
             super().__init__(master)
             self.logReport = reportLog.ReportLog()
-            if not os.path.isfile('arm_1.png'):
-                print(f"Error: The file does not exist")
-                sys.exit(1)
-
-            self.loaderImage = loadImage.LoadImage(path='app/images/fracture.jpg')
+            self.loaderImage = loadImage.LoadImage(path='Images/fracture.jpg')
             self.master = master
             self.width = 1080
             self.height = 720
-            self.master.geometry("%dx%d" % (self.width, self.height))
+            self.master.geometry("%dx%d" % (self.width, self.height)) # establecer tamaño de la ventana
             self.pack
             self.panel = None
             self.bandImage = False
@@ -35,13 +32,18 @@ class App(ttk.Frame):
             self.y1 = -1
             self.x2 = -1
             self.y2 = -1
+            self.x11 = -1
+            self.y11 = -1
+            self.x22 = -1
+            self.y22 = -1
             self.band1 = False
             self.band2 = True
+            self.event_click1 = self.event_click2
 
             self.createWidgets()
             self.createFrameZeros()
             self.logReport.logger.info("GUI created")
-            self.master.mainloop()            
+            self.master.mainloop()     # iniciar el bucle de eventos 
 
         except Exception as e:
             self.logReport.logger.error("GUI no created" + str(e))
@@ -65,14 +67,15 @@ class App(ttk.Frame):
         self.lblVideo2.configure(image = imgTk2)
         self.lblVideo2.image = imgTk2
 
-        self.lblVideo3 = tk.Label(self.master, borderwidth=2, relief="solid")
+        self.lblVideo3 = tk.Canvas(self.master, width=300, height=300, borderwidth=2, relief="solid")
         self.lblVideo3.place(x=640, y=20)
         frame3 = np.zeros([300, 300, 3], dtype=np.uint8)
         frame3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2RGB)
-        imgArray3 = Image.fromarray(frame3)
-        imgTk3 = ImageTk.PhotoImage(image=imgArray3)
-        self.lblVideo3.configure(image = imgTk3)
-        self.lblVideo3.image = imgTk3
+        self.imgTk3 = ImageTk.PhotoImage(image=Image.fromarray(frame3))
+        self.lblVideo3.create_image(150, 150, image=self.imgTk3)
+
+        
+        
     
     def createWidgets(self):
         self.fontText = font.Font(family='Helvetica', size=8, weight = 'normal')
@@ -119,6 +122,7 @@ class App(ttk.Frame):
                                        width=12,
                                        command=self.closeProcess)
         self.btnCloseProcess.place(x=950, y=230)
+        
 
         self.sliderL = Scale(self.master, from_=0, to=256, orient=tk.HORIZONTAL, command=self.slider_callback)
         self.sliderL.place(x=20, y=330)
@@ -140,6 +144,7 @@ class App(ttk.Frame):
             if(self.loaderImage.imgGray.any() is not None):
                 self.loaderImage.getBinaryImageIR(self.sliderL.get(), self.sliderR.get())
                 self.drawBinaryImage()
+        
     
 
     def InitProcess(self):
@@ -156,9 +161,15 @@ class App(ttk.Frame):
 
     def dilateProcess(self):
         print('dilate')
+        self.loaderImage.dilateImage(kernel = np.ones((5,5), np.uint8))
+        self.drawBinaryImage()
+        
 
     def filterProcess(self):
         print('filter')
+        self.loaderImage.filterImage(kernel = np.ones((5,5), np.float32)/25)
+        self.drawBinaryImage()
+        
     
     def closeProcess(self):
         response = messagebox.askquestion("Salir", "¿Estás seguro de que quieres salir?")
@@ -207,18 +218,43 @@ class App(ttk.Frame):
             print("Label clicked at x2={}, y2={}".format(self.x2, self.y2))
             self.loaderImage.drawROI(self.x1, self.y1, self.x2, self.y2, self.sliderScale.get())
             self.drawRoiImage()
+            
+    def event_click3(self, event):
+        # Verifica si es el primer o segundo clic
+        if not self.band1:
+            self.x11, self.y11 = event.x, event.y
+            self.band1 = True
+            self.band2 = False
+            print("Primer punto en x={}, y={}".format(self.x11, self.y11))
+        elif not self.band2:
+            self.x22, self.y22 = event.x, event.y
+            self.band2 = True
+            self.band1 = False
+            print("Segundo punto en x={}, y={}".format(self.x22, self.y22))
+            self.draw_line_and_calculate_distance()
+            #replce the actual ROI image with the new image with the line
+            #self.drawRoiImage()
+            
+
+    def draw_line_and_calculate_distance(self):
+        if self.loaderImage.imgROI.any():
+            # Dibuja la línea sobre el lienzo
+            self.lblVideo3.create_line(self.x11, self.y11, self.x22, self.y22, fill="red", width=2)
+            
+            # Calcula la distancia en píxeles
+            distancia = math.sqrt((self.x22 - self.x11) ** 2 + (self.y22 - self.y11) ** 2)
+            print("Distancia en píxeles:", distancia)
+
 
     def drawRoiImage(self):
         try:
-            if(self.loaderImage.imgROI.any() is not None):
+            if self.loaderImage.imgROI.any():
                 img3 = self.loaderImage.imgROI
-                frame3 = cv2.resize(img3, (300,300))
+                frame3 = cv2.resize(img3, (300, 300))
                 frame3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2RGB)
-                imgArray3 = Image.fromarray(frame3)
-                imgTk3 = ImageTk.PhotoImage(image=imgArray3)
-                self.lblVideo3.configure(image = imgTk3)
-                self.lblVideo3.image = imgTk3
-                #self.lblVideo3.bind("<Button-1>", self.event_click3)
+                self.imgTk3 = ImageTk.PhotoImage(image=Image.fromarray(frame3))
+                self.lblVideo3.create_image(150, 150, image=self.imgTk3)  # Actualiza la imagen en el canvas
+                self.lblVideo3.bind("<Button-1>", self.event_click3)
         except Exception as e:
             self.logReport.logger.info("Error in get binary image " + str(e))
         
