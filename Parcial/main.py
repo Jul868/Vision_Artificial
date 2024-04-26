eadce459048764a6cca82b594ea35a068aeb4826
Jul868 <julianaa2708@gmail.com>
2024-04-26 00:38:07 -0500

    F

--- Parcial/main.py
+++
@@ -1,7 +0,0 @@
-import gui
-import reportlog
-
-if __name__=="__main__":
-    logReport = reportlog.ReportLog() # Solo recorre el init de la clase creada 
-    logReport.logger.info("Init main") # Inicia el prgrama 
-    gui.main()
